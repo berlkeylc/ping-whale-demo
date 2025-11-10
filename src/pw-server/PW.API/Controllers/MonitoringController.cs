56de9e09577601c322b27ff51d34e8e10ddd6219
berke <berkeyalcner@gmail.com>
2025-11-10 16:15:01 +0300

    Monitoring Controller changes

--- src/pw-server/PW.API/Controllers/MonitoringController.cs
+++ src/pw-server/PW.API/Controllers/MonitoringController.cs
@@ -1,5 +1,6 @@
 using System;
 using MediatR;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PW.Application.Features.Monitoring.GetMonitors;
@@ -7,25 +8,22 @@ using PW.Application.Features.Monitoring.SaveMonitor;
 
 namespace PW.API.Controllers;
 
-    [Authorize]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     [ApiController]
     public class MonitoringController : Controller
     {
-        readonly IMediator _mediator;
-    public MonitoringController(IMediator mediator)
-    {
-        _mediator = mediator;
-    }
-        
-        private Guid GetUserId() =>
-    Guid.Parse(User.FindFirst("sub")?.Value ?? throw new Exception("UserId not found"));
-     
-
-        [HttpGet("{id?}")]
-        public async Task<IActionResult> Get(GetMonitorsRequest request)
+    readonly IMediator _mediator;
+        public MonitoringController(IMediator mediator)
         {
-           GetMonitorsResponse response = await _mediator.Send(request);
+        _mediator = mediator;
+        }
+        
+        [HttpGet("{id?}")]
+        public async Task<IActionResult> Get(Guid? id)
+        {
+            var request = new GetMonitorsRequest(){ MonitorId = id };
+            GetMonitorsResponse response = await _mediator.Send(request);
             return Ok(response);
         }
 
