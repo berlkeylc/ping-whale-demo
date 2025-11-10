f9810e535d2ec7344e93cdb4af8808cbc9662df6
berke <berkeyalcner@gmail.com>
2025-11-10 16:50:47 +0300

    Landing Page impl.

---
+++ src/pw-client/src/app/pages/landing/landing.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: 'landing.html',
+})
+export class LandingComponent {}
