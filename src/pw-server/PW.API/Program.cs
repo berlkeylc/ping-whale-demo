c6b318d41fdbd454ccef569f43931d11b98feae3
berke <berkeyalcner@gmail.com>
2025-11-10 16:19:48 +0300

    program.cs

--- src/pw-server/PW.API/Program.cs
+++ src/pw-server/PW.API/Program.cs
@@ -1,49 +1,117 @@
 
 using PW.Persistence;
 using DotNetEnv;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Reflection;
+using PW.Application.Features.Auth.Register;
+using PW.API.Extensions;
+using PW.Infrastructure;
 
 Env.Load();
 var builder = WebApplication.CreateBuilder(args);
+
+//Define CORS policy to allow Angular client
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAngularClient",
+        policy =>
+        {
+            policy
+                .WithOrigins("http://localhost:4200") // Angular app adresi
+                .WithOrigins("https://localhost:4200")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+//Define CORS policy to allow Angular client
+
+//Configure MediatR
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssemblies(
+        Assembly.GetExecutingAssembly(),
+        typeof(CreateUserHandler).Assembly // Handler assembly
+    );
+});
+//Configure MediatR
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+/// For Swagger UI
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+/// For Swagger UI
+
+// For Auth JWT Configuration
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+// For Auth JWT Configuration
+
+/// Add services to the container.
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddPersistenceServices(builder.Configuration);
+builder.Services.AddInfrastructureServices();
+/// Add services to the container.
+
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-builder.Services.AddPersistenceServices(builder.Configuration);
+
 
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    //For Swagger UI 
+    app.UseSwagger();
+    app.UseSwaggerUI();
+    //For Swagger UI 
 }
+
+// Global Exception Handling Middleware
+app.ConfigureExceptionHandler<Program>(app.Services.GetRequiredService<ILogger<Program>>());
+// Global Exception Handling Middleware
+
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
+// Apply CORS policy
+app.UseCors("AllowAngularClient");
+// Apply CORS policy
+
+// For Auth 
+app.UseAuthentication();
+app.UseAuthorization();
+// For Auth
+
+app.MapControllers();
 
 app.Run();
 
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
